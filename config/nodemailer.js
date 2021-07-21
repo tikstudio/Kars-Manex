72dc3fe937a98b0042a8ae81b859b0ce4935672a
KarsManex <87611757+KarsManex@users.noreply.github.com>
2021-07-21 04:19:53 +0400

    Update nodemailer.js

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -7,12 +7,10 @@ const {
 const transporter = nodemailer.createTransport({
   host: NODEMAILER_HOST,
   port: NODEMAILER_PORT,
+  secure: NODEMAILER_SECURE,
   auth: {
     user: NODEMAILER_USER,
     pass: NODEMAILER_PASS,
-  },
-  tls: {
-    rejectUnauthorized: false,
   },
 });
 
